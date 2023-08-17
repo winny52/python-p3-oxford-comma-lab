d314a48780c88e0d9f631945f53fe113398e8da1
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-17 20:52:58 +0300

    Completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 0:
+        return ""
+    elif len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        comma_separated = ", ".join(elements[:-1])
+        return f"{comma_separated}, and {elements[-1]}"
+
+# Test the function
+elements = ["fiddleheads", "okra", "kohlrabi"]
+result = oxford_comma(elements)
+print(result)  # Output: "fiddleheads, okra, and kohlrabi"
